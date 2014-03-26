332bcacbe65d582fb1e15715c8d7e568103138c4
bstocks <ben@Benjamins-MacBook-Pro.local>
2014-03-25 17:57:09 -0700

    add hellobar

--- assets/js/reaction.js
+++ assets/js/reaction.js
@@ -1,25 +1,26 @@
-$('#sidebar').affix({
-      offset: {
-        top: 150,
-        bottom: function () {
-            return (this.bottom = $('footer').outerHeight(true) + 15)
-        }
-      }
-});
-
-var $body   = $(document.body);
-
-$body.scrollspy({
-	target: '#sidebar',
-	offset: 5
-});
-
-/* Carousel */
 $(document).ready(function() {
-	$("#main-carousel").owlCarousel({
- 		
- 		items : 1,
- 		itemsDesktop : [1199,1],
+
+    $('#sidebar').affix({
+          offset: {
+            top: 150,
+            bottom: function () {
+                return (this.bottom = $('footer').outerHeight(true) + 15)
+            }
+          }
+    });
+
+    var $body   = $(document.body);
+
+    $body.scrollspy({
+    	target: '#sidebar',
+    	offset: 5
+    });
+
+    /* Carousel */
+    $("#main-carousel").owlCarousel({
+    		
+    		items : 1,
+    		itemsDesktop : [1199,1],
     itemsDesktopSmall : [980,1],
     itemsTablet: [768,1],
     itemsMobile : [479,1],
@@ -30,57 +31,57 @@ $(document).ready(function() {
     itemsTabletSmall: false,
     singleItem : false,
     itemsScaleUp : false,
- 
+
     //Basic Speeds
     slideSpeed : 200,
     paginationSpeed : 800,
     rewindSpeed : 1000,
- 
+
     //Autoplay
     autoPlay : false,
     stopOnHover : false,
- 
+
     // Navigation
     navigation : false,
     navigationText : ["prev","next"],
     rewindNav : true,
     scrollPerPage : false,
- 
+
     //Pagination
     pagination : true,
     paginationNumbers: false,
- 
+
     // Responsive 
     responsive: true,
     responsiveBaseWidth: window,
- 
+
     // CSS Styles
     baseClass : "owl-carousel",
     theme : "owl-theme",
- 
+
     //Lazy load
     lazyLoad : false,
     lazyFollow : true,
     lazyEffect : "fade",
- 
+
     //Auto height
     autoHeight : false,
- 
+
     //JSON 
     jsonPath : false, 
     jsonSuccess : false,
- 
+
     //Mouse Events
     dragBeforeAnimFinish : true,
     mouseDrag : true,
     touchDrag : true,
- 
+
     //Transitions
     transitionStyle : false,
- 
+
     // Other
     addClassActive : false,
- 
+
     //Callbacks
     beforeUpdate : false,
     afterUpdate : false,
@@ -91,6 +92,10 @@ $(document).ready(function() {
     afterAction: false,
     startDragging : false,
     afterLazyLoad : false
- 
-	});
+
+    });
+
+    /* hellobar */
+    $(".hellobar").delay(500).slideDown();
+
 });
